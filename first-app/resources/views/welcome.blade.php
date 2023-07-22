37f05c0133e803b927a3e87226cb3816a51350d7
giovani7cruz <giovani7cruz@gmail.com>
2023-07-22 19:44:06 -0300

    teste

--- first-app/resources/views/welcome.blade.php
+++ first-app/resources/views/welcome.blade.php
@@ -6,11 +6,7 @@
 
         <title>First Project Laravel - Giovani Da Cruz</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-        <!-- Styles -->
+        
         
     </head>
     <body>
