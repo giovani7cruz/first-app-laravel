3a1a604ca43a1573dc8299d87ebc0bb5be50487b
giovani7cruz <giovani7cruz@gmail.com>
2023-07-21 21:20:14 -0300

    Fim da aula 5.
    For, Foreach, php puro e comentários no blade.

--- first-app/resources/views/welcome.blade.php
+++ first-app/resources/views/welcome.blade.php
@@ -17,18 +17,45 @@
         <h1>I'm happpy! This is my first laravel app</h1>
         
         <h2>Treinando títulos</h2>
+        
+        {{-- Exemplo de if simples --}}
         @if(10 > 5)
             <p>A condição é True</p>
         @endif
 
         <p>{{$nome}}</p>
 
+        {{-- Exemplo de if, ifelse e else --}}
         @if($nome == "Pedro")
             <p>O Nome é Pedro</p>
         @elseif($nome == "Giovani Da Cruz")
             <p>O Nome é {{$nome}} e ele tem {{$idade}} anos e trabalha como {{$profissao}}.</p>
         @else
             <p>Teste: {{$nome}}</p>
-        @endif    
+        @endif   
+        
+        {{-- Exemplo de loop for --}}
+        Exemplo de loop for
+        @for($i = 0; $i < count($arr); $i++)
+            <p>{{ $arr[$i]}} - {{$i}}</p>
+            @if( $i == 2 )
+                <p>O i é igual a 2!</p>
+            @endif
+        @endfor
+
+        {{-- Exemplo de Foreach no blade --}}
+        Exemplo de Foreach no blade
+        @foreach($nomes as $nome)
+            <p>{{$loop->index}}</p>
+            <p>{{$nome}}</p>
+        @endforeach
+
+        {{-- Exemplo de php puro no blade --}}
+        Exemplo de php puro no blade 
+        @php
+            $name = "João escrito em php puro";
+
+            echo $name;
+        @endphp
     </body>
 </html>
--- first-app/routes/web.php
+++ first-app/routes/web.php
@@ -18,9 +18,19 @@ Route::get('/', function () {
     $idade = 33;
     $profissao = "Programador";
 
+    $arr = [10, 20, 30, 40, 50];
+
+    $nomes = ["Giovani", "João", "José", "Joaquim"];
+
     /* O que se usa nas blades é o nome da chave do elemento no array */ 
-    return view('welcome', ['nome' => $nome, 'idade' => $idade, 
-    'profissao' => $profissao]);
+    return view('welcome', 
+        [
+            'nome' => $nome, 
+            'idade' => $idade, 
+            'profissao' => $profissao,
+            'arr' => $arr,
+            'nomes' => $nomes
+        ]);
 });
 
 Route::get('/first', function () {
